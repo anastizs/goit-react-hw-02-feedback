7694619f3d1b1a7c2b3b68760c90155b80958718
Anastasiia <dessertkiev@gmail.com>
2023-04-12 10:46:53 +0300

    feedback 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,52 @@
-import React from "react";
-import Feedback from "./Feedback/Feedback";
+import React, { Component } from "react";
+import ControlsBtn from "./Controls/Controls";
 
-const App = () => {
-  return (
-    <div>
-      <Feedback/>
-    </div>
-  );
-};
+class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
-export default App;
+  feedbackHandler = (option) => {
+    this.setState((prevState) => {
+      return {
+        [option]: prevState[option] + 1,
+      };
+    });
+  };
+
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+  countPositiveFeedbackPercentage = () => {
+    return Math.floor((100 / this.countTotalFeedback()) * this.state.good);
+  };
+
+  render() {
+    const total = this.countTotalFeedback();
+    const positiveFeedbackPercentage = this.countPositiveFeedbackPercentage();
+
+    return (
+      <div>
+        <h1>Please leave feedback</h1>
+        <ControlsBtn
+          options={Object.keys(this.state)}
+          onLeaveFeedback={this.feedbackHandler}
+        />
+        <div>
+          <h2>Statistics</h2>
+          <ul>
+            <li>Good: {this.state.good}</li>
+            <li>Neutral: {this.state.neutral}</li>
+            <li>Bad: {this.state.bad}</li>
+            <li>Total: {total}</li>
+            <li>Positive feedback: {positiveFeedbackPercentage}%</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+export default Feedback;
